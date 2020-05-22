5e0b733532b0aa4b46020e05f8bb67764da5e92f
David Adams <dadams2212@gmail.com>
2020-05-22 11:45:52 -0400

    Create initial models and make their corresponding migrations

--- inventory_management/models.py
+++ inventory_management/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Site(models.Model):
+    """ Represents a single customer site/restaurant """
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class InventoryItem(models.Model):
+    """ Represents an item in a customer's inventory """
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    associated_site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    last_measurement = models.FloatField()
+    last_measurement_timestamp = models.DateTimeField()
+
+
+class Scale(models.Model):
+    """ Represents a scale that can be used for measurements """
+    associated_site = models.ForeignKey(Site, null=True, on_delete=models.SET_NULL)
+    associated_item = models.ForeignKey(InventoryItem, null=True, on_delete=models.SET_NULL)
+
+
+class Measurement(models.Model):
+    """ Represents a single measurement made by a single scale """
+    value = models.FloatField()
+    scale = models.ForeignKey(Scale, on_delete=models.PROTECT)
