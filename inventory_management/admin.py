40c8aebf6a7fce25d99c4536d7ae40dedbada80e
David Adams <dadams2212@gmail.com>
2020-05-22 17:31:50 -0400

    Register all of the models with the admin site

--- inventory_management/admin.py
+++ inventory_management/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Site
+from .models import Site, InventoryItem, Scale, Measurement
 
 
 admin.site.register(Site)
+admin.site.register(InventoryItem)
+admin.site.register(Scale)
+admin.site.register(Measurement)
