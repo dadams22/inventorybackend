0f005398c2e2dad913e8e065000783ddf389db74
David Adams <dadams2212@gmail.com>
2020-05-22 21:38:06 -0400

    Allow InventoryItem fields last_measurement and last_measurement_timestamp to be blank so admin site does not require them

--- inventory_management/models.py
+++ inventory_management/models.py
@@ -16,8 +16,8 @@ class InventoryItem(models.Model):
     description = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     associated_site = models.ForeignKey(Site, on_delete=models.CASCADE)
-    last_measurement = models.FloatField(null=True)
-    last_measurement_timestamp = models.DateTimeField(null=True)
+    last_measurement = models.FloatField(null=True, blank=True)
+    last_measurement_timestamp = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.name
