6297027a11cbe9bcaca50b2126a488a1b60dbbb9
David Adams <dadams2212@gmail.com>
2020-08-07 10:22:12 -0400

    Register ItemStocking with the admin site

--- inventory_management/admin.py
+++ inventory_management/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Site, Profile, InventoryItem, Scale, ScaleReading
+from .models import Site, Profile, InventoryItem, ItemStocking, Scale, ScaleReading
 
 
 admin.site.register(Site)
 admin.site.register(Profile)
 admin.site.register(InventoryItem)
+admin.site.register(ItemStocking)
 admin.site.register(Scale)
 admin.site.register(ScaleReading)
